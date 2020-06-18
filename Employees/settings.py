5fd4f786a9558fed673420f5e0360e918c55667a
Ratnam123 <venkatratnam45@gmail.com>
2020-06-18 22:37:01 +0530

    created new app, Stats

--- Employees/settings.py
+++ Employees/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'Employees.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Stats',
+        'USER': 'root',
+        'PASSWORD': 'admin',
+        'PORT': 3306
     }
 }
 
